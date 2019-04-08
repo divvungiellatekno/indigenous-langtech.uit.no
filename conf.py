20364fdb3d9662884738c11d96ebca13aa6f7cb8
Børre Gaup <albbas@gmail.com>
2019-04-08 17:20:48 +0000

    Deploy command for indigenous-langtech

--- conf.py
+++ conf.py
@@ -625,11 +625,11 @@ REDIRECTIONS = []
 # to `nikola deploy`.  If no arguments are specified, a preset
 # named `default` will be executed.  You can use as many presets
 # in a `nikola deploy` command as you like.
-# DEPLOY_COMMANDS = {
-#     'default': [
-#         "rsync -rav --delete output/ joe@my.site:/srv/www/site",
-#     ]
-# }
+DEPLOY_COMMANDS = {
+    'default': [
+        "rsync -rav --delete output/ bga001@divvun.uit.no:/www/divvun.uit.no/htdocs",
+    ]
+}
 
 # github_deploy configuration
 # For more details, read the manual:
