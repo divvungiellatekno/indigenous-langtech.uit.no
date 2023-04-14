36f13929ce59abe1817d18cc1018d65108f83278
Børre Gaup <albbas@gmail.com>
2023-04-14 10:56:54 +0200

    Formatting by VSCode

--- conf.py
+++ conf.py
@@ -109,7 +109,7 @@ TRANSLATIONS = {
 # this pattern is also used for metadata:
 #     something.meta -> something.pl.meta
 
-TRANSLATIONS_PATTERN = '{path}.{lang}.{ext}'
+TRANSLATIONS_PATTERN = "{path}.{lang}.{ext}"
 
 # Links for the sidebar / navigation bar.  (translatable)
 # This is a dict.  The keys are languages, and values are tuples.
@@ -138,20 +138,32 @@ TRANSLATIONS_PATTERN = '{path}.{lang}.{ext}'
 
 
 NAVIGATION_LINKS = {
-        DEFAULT_LANG: (
-            ('/index.html', 'Home', 'fa fa-home'),
-            ('/blog/', 'Blog', 'fa fa-folder-open'),
-            ('https://giellalt.github.io', 'GiellaLT Home Page', 'fab fa-user'),
-            ('https://giella.zulipchat.com', 'GiellaLT Zulip Chats', 'fab fa-zulip'),
-            ('https://www.facebook.com/Divvun/', 'Divvuns Facebook page', 'fab fa-facebook'),
-            ('https://www.instagram.com/divvun.no/', 'Divvuns Instagram page', 'fab fa-instagram'),
-            ('https://twitter.com/divvun', 'Divvuns Twitter home', 'fab fa-twitter'),
-            ('https://www.youtube.com/channel/UCkV9RzELCwl0SigSZpTif1A', 'Divvuns YouTube channel', 'fab fa-youtube'),
-            ('https://github.com/divvun', 'Divvun at github', 'fab fa-github'),
-            ('/categories/index.html', 'Tags', 'fa fa-tags'),
-            ('/rss.xml', 'RSS', 'fa fa-rss'),
-        )
-    }
+    DEFAULT_LANG: (
+        ("/index.html", "Home", "fa fa-home"),
+        ("/blog/", "Blog", "fa fa-folder-open"),
+        ("https://giellalt.github.io", "GiellaLT Home Page", "fab fa-user"),
+        ("https://giella.zulipchat.com", "GiellaLT Zulip Chats", "fab fa-zulip"),
+        (
+            "https://www.facebook.com/Divvun/",
+            "Divvuns Facebook page",
+            "fab fa-facebook",
+        ),
+        (
+            "https://www.instagram.com/divvun.no/",
+            "Divvuns Instagram page",
+            "fab fa-instagram",
+        ),
+        ("https://twitter.com/divvun", "Divvuns Twitter home", "fab fa-twitter"),
+        (
+            "https://www.youtube.com/channel/UCkV9RzELCwl0SigSZpTif1A",
+            "Divvuns YouTube channel",
+            "fab fa-youtube",
+        ),
+        ("https://github.com/divvun", "Divvun at github", "fab fa-github"),
+        ("/categories/index.html", "Tags", "fa fa-tags"),
+        ("/rss.xml", "RSS", "fa fa-rss"),
+    )
+}
 
 
 DATE_FANCINESS = 2
@@ -159,16 +171,14 @@ DATE_FANCINESS = 2
 # Alternative navigation links. Works the same way NAVIGATION_LINKS does,
 # although themes may not always support them. (translatable)
 # (Bootstrap 4: right-side of navbar, Bootblog 4: right side of title)
-NAVIGATION_ALT_LINKS = {
-    DEFAULT_LANG: {}
-}
+NAVIGATION_ALT_LINKS = {DEFAULT_LANG: {}}
 
 # Name of the theme to use.
 THEME = "zen"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
-THEME_COLOR = '#5670d4'
+THEME_COLOR = "#5670d4"
 
 # Theme configuration. Fully theme-dependent. (translatable)
 # Examples below are for bootblog4.
@@ -178,18 +188,18 @@ THEME_COLOR = '#5670d4'
 THEME_CONFIG = {
     DEFAULT_LANG: {
         # Show the latest featured post in a large box, with the previewimage as its background.
-        'featured_large': False,
+        "featured_large": False,
         # Show the first (remaining) two featured posts in small boxes.
-        'featured_small': False,
+        "featured_small": False,
         # Show featured posts on mobile.
-        'featured_on_mobile': True,
+        "featured_on_mobile": True,
         # Show image in `featured_large` on mobile.
         # `featured_small` displays them only on desktop.
-        'featured_large_image_on_mobile': True,
+        "featured_large_image_on_mobile": True,
         # Strip HTML from featured post text.
-        'featured_strip_html': False,
+        "featured_strip_html": False,
         # Contents of the sidebar, If empty, the sidebar is not displayed.
-        'sidebar': ''
+        "sidebar": "",
     }
 }
 
@@ -285,7 +295,7 @@ TIMEZONE = "Europe/Oslo"
 # One or more folders containing files to be copied as-is into the output.
 # The format is a dictionary of {source: relative destination}.
 # Default is:
-FILES_FOLDERS = {'files': ''}
+FILES_FOLDERS = {"files": ""}
 # Which means copy 'files' into 'output'
 
 # One or more folders containing code listings to be processed and published on
@@ -302,18 +312,18 @@ FILES_FOLDERS = {'files': ''}
 # 'markdown' is Markdown
 # 'html' assumes the file is HTML and just copies it
 COMPILERS = {
-    "rest": ('.rst', '.txt'),
-    "markdown": ('.md', '.mdown', '.markdown'),
-    "textile": ('.textile',),
-    "txt2tags": ('.t2t',),
-    "bbcode": ('.bb',),
-    "wiki": ('.wiki',),
-    "ipynb": ('.ipynb',),
-    "html": ('.html', '.htm'),
+    "rest": (".rst", ".txt"),
+    "markdown": (".md", ".mdown", ".markdown"),
+    "textile": (".textile",),
+    "txt2tags": (".t2t",),
+    "bbcode": (".bb",),
+    "wiki": (".wiki",),
+    "ipynb": (".ipynb",),
+    "html": (".html", ".htm"),
     # PHP files are rendered the usual way (i.e. with the full templates).
     # The resulting files have .php extensions, making it possible to run
     # them without reconfiguring your server to recognize them.
-    "php": ('.php',),
+    "php": (".php",),
     # Pandoc detects the input from the source filename
     # but is disabled by default as it would conflict
     # with many of the others.
@@ -398,7 +408,7 @@ COMPILERS = {
 # If you do not want to display a tag publicly, you can mark it as hidden.
 # The tag will not be displayed on the tag list page and posts.
 # Tag pages will still be generated.
-HIDDEN_TAGS = ['mathjax']
+HIDDEN_TAGS = ["mathjax"]
 
 # Only include tags on the tag list/overview page if there are at least
 # TAGLIST_MINIMUM_POSTS number of posts or more with every tag. Every tag
@@ -543,7 +553,7 @@ HIDDEN_CATEGORIES = []
 # If you do not want to display an author publicly, you can mark it as hidden.
 # The author will not be displayed on the author list page and posts.
 # Tag pages will still be generated.
-HIDDEN_AUTHORS = ['Guest']
+HIDDEN_AUTHORS = ["Guest"]
 
 # Final location for the main blog page and sibling paginated pages is
 # output / TRANSLATION[lang] / INDEX_PATH / index-*.html
@@ -552,9 +562,7 @@ HIDDEN_AUTHORS = ['Guest']
 
 # Optional HTML that displayed on “main” blog index.html files.
 # May be used for a greeting. (translatable)
-FRONT_INDEX_HEADER = {
-    DEFAULT_LANG: ''
-}
+FRONT_INDEX_HEADER = {DEFAULT_LANG: ""}
 
 # Create per-month archives instead of per-year
 # CREATE_MONTHLY_ARCHIVE = False
@@ -639,7 +647,7 @@ REDIRECTIONS = []
 # named `default` will be executed.  You can use as many presets
 # in a `nikola deploy` command as you like.
 DEPLOY_COMMANDS = {
-    'default': [
+    "default": [
         "rsync -rav --delete output/ /www/indigenous-langtech.uit.no/htdocs",
     ]
 }
@@ -648,11 +656,11 @@ DEPLOY_COMMANDS = {
 # For more details, read the manual:
 # https://getnikola.com/handbook.html#deploying-to-github
 # You will need to configure the deployment branch on GitHub.
-GITHUB_SOURCE_BRANCH = 'main'
-GITHUB_DEPLOY_BRANCH = 'gh-pages'
+GITHUB_SOURCE_BRANCH = "main"
+GITHUB_DEPLOY_BRANCH = "gh-pages"
 
 # The name of the remote where you wish to push to, using github_deploy.
-GITHUB_REMOTE_NAME = 'origin'
+GITHUB_REMOTE_NAME = "origin"
 
 # Whether or not github_deploy should commit to the source branch automatically
 # before deploying.
@@ -817,7 +825,7 @@ GITHUB_COMMIT_SOURCE = False
 # (the thumbnail has ``.thumbnail`` added before the file extension by default,
 # but a different naming template can be configured with IMAGE_THUMBNAIL_FORMAT).
 
-IMAGE_FOLDERS = {'images': 'images'}
+IMAGE_FOLDERS = {"images": "images"}
 # IMAGE_THUMBNAIL_SIZE = 400
 # IMAGE_THUMBNAIL_FORMAT = '{name}.thumbnail{ext}'
 
@@ -962,15 +970,15 @@ CONTENT_FOOTER_FORMATS = {
             "email": BLOG_EMAIL,
             "author": BLOG_AUTHOR,
             "date": time.gmtime().tm_year,
-            "license": LICENSE
-        }
+            "license": LICENSE,
+        },
     )
 }
 
 # A simple copyright tag for inclusion in RSS feeds that works just
 # like CONTENT_FOOTER and CONTENT_FOOTER_FORMATS
 RSS_COPYRIGHT = 'Contents © {date} <a href="mailto:{email}">{author}</a> {license}'
-RSS_COPYRIGHT_PLAIN = 'Contents © {date} {author} {license}'
+RSS_COPYRIGHT_PLAIN = "Contents © {date} {author} {license}"
 RSS_COPYRIGHT_FORMATS = CONTENT_FOOTER_FORMATS
 
 # To use comments, you can choose between different third party comment
@@ -1085,7 +1093,11 @@ PRETTY_URLS = True
 #       with the MarkdownExtension class and should not be added here.
 # Defaults are markdown.extensions.(fenced_code|codehilite|extra)
 # markdown.extensions.meta is required for Markdown metadata.
-MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.codehilite', 'markdown.extensions.extra']
+MARKDOWN_EXTENSIONS = [
+    "markdown.extensions.fenced_code",
+    "markdown.extensions.codehilite",
+    "markdown.extensions.extra",
+]
 
 # Options to be passed to markdown extensions (See https://python-markdown.github.io/reference/)
 # Default is {} (no config at all)
