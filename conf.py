aea6333613e0a95443f90aa22ae0a3dd9c8e9460
Sjur N Moshagen <sjurnm@mac.com>
2023-04-14 12:43:14 +0300

    Do not deploy on GH anymore

--- conf.py
+++ conf.py
@@ -661,7 +661,7 @@ DEPLOY_COMMANDS = {
 # https://getnikola.com/handbook.html#deploying-to-github
 # You will need to configure the deployment branch on GitHub.
 GITHUB_SOURCE_BRANCH = "main"
-GITHUB_DEPLOY_BRANCH = "gh-pages"
+# GITHUB_DEPLOY_BRANCH = "gh-pages"
 
 # The name of the remote where you wish to push to, using github_deploy.
 GITHUB_REMOTE_NAME = "origin"
